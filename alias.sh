654932de5b2bdd8eafeae31a9658e12f36ef1123
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2015-05-25 14:44:04 -0300

    Remove alias install Magento

--- alias.sh
+++ alias.sh
@@ -3,7 +3,6 @@ base="~/Simple-Developer-Alias"
 
 # Alias Install
 alias install-magerun="cd $base;mkdir tools; cd tools;wget https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar; chmod +x ./n98-magerun.phar; cp ./n98-magerun.phar /usr/local/bin/"
-alias install-magento="bash ./Magento/magento-install.sh"
 
 # Alias Tools
 alias magerun="$base/tools/n98-magerun.phar"
@@ -11,7 +10,8 @@ alias modgit="$base/tools/modgit"
 
 # Alias Git
 alias gitst="clear;git status"
-alias limpa="git clean -df;git checkout -- . ;git pull --all;clear; magerun index:reindex:all;magerun cache:flush;magerun cache:clean;git status; rm -rf var/cache/*"
+alias limpa="git reset --hard; git clean -df;git checkout -- . ;git pull --all;clear; magerun index:reindex:all;magerun cache:flush;magerun cache:clean;git status; 
+rm -rf var/cache/*"
 
 # Alias System
 alias 777="chmod -Rv 777"
