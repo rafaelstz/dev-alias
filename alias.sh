99d66087cb522d9cf1c29631fcb84ec60b5a7125
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2015-07-29 00:56:17 -0300

    Upgrade alias www

--- alias.sh
+++ alias.sh
@@ -25,7 +25,7 @@ alias nc="n98 cache:clean;n98 cache:flush"
 alias nr="n98 index:reindex:all"
 
 # Alias System
-alias www="cd /var/www/html/"
+alias www="clear; cd /var/www/html/; ls -lh;"
 alias 777="chmod -Rv 777"
 alias 755="chmod -Rv 755"
 alias 644="chmod -Rv 644"
