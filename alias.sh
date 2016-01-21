dafb75a422bc5a73bf08414fbcd1fbbc9638ad3d
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2016-01-21 13:47:58 -0200

    Add Fetch Tags

--- alias.sh
+++ alias.sh
@@ -17,7 +17,7 @@ alias modgit="$base/tools/modgit"
 
 # Alias Git
 alias gitst="clear;git status"
-alias resetar="git reset --hard; git reset HEAD; git clean -df;git checkout -- . ;git pull --all;git pull --tags;clear; magerun cache:flush;magerun cache:clean;clear;git status"
+alias resetar="git reset --hard; git reset HEAD; git clean -df;git checkout -- . ;git pull --all;git fetch --tags;git pull --tags;clear; magerun cache:flush;magerun cache:clean;clear;git status"
 alias limpa="resetar;resetar"
 alias gitlog="git log $logview- --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 
