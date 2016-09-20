92139e2c13b32a9fc632cd5057fe1170eba7e563
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2016-09-20 16:31:35 -0300

    Add Hypernode on Magerun

--- alias.sh
+++ alias.sh
@@ -6,10 +6,10 @@ logview="5"
 alias upgrade-devalias="cd $base;rm master.zip;wget https://github.com/rafaelstz/dev-alias/archive/master.zip;unzip -qo master.zip;mv dev-alias-master/* ./;rm -rf dev-alias-master;rm master.zip; source ~/.bashrc; source ~/.zshrc; clear; cd ~/"
 
 # Alias Install Dependecy
-alias install-base="sudo apt-get install -y wget unzip"
+alias install-base="sudo apt-get install -y git wget unzip"
 
 # Alias Install
-alias install-magerun="install-base; cd $base;mkdir tools; cd tools; rm n98-magerun.phar;wget https://files.magerun.net/n98-magerun.phar; chmod +x ./n98-magerun.phar; cp ./n98-magerun.phar /usr/local/bin/; mkdir -p ~/.n98-magerun/modules/; cd ~/.n98-magerun/modules/ && git clone https://github.com/peterjaap/magerun-addons.git ; clear; cd ~/"
+alias install-magerun="install-base; cd $base;mkdir tools; cd tools; rm n98-magerun.phar;wget https://files.magerun.net/n98-magerun.phar; chmod +x ./n98-magerun.phar; cp ./n98-magerun.phar /usr/local/bin/; mkdir -p ~/.n98-magerun/modules/; cd ~/.n98-magerun/modules/ && git clone https://github.com/peterjaap/magerun-addons.git; git clone https://github.com/Hypernode/hypernode-magerun.git ; clear; cd ~/"
 alias install-magerun2="install-base; cd $base;mkdir tools; cd tools; rm n98-magerun2.phar;wget https://files.magerun.net/n98-magerun2.phar; chmod +x ./n98-magerun2.phar; cp ./n98-magerun2.phar /usr/local/bin/ ; clear; cd ~/"
 alias install-modgit="install-base; cd $base;mkdir tools; cd tools;rm modgit;wget -O modgit https://raw.github.com/jreinke/modgit/master/modgit; chmod +x modgit; cp modgit /usr/local/bin/;clear;cd ~/"
 alias install-composer="install-base; cd $base;mkdir tools; cd tools; composer.phar;sudo apt-get install curl -y;curl -sS https://getcomposer.org/installer | sudo php ; chmod +x composer.phar; cp composer.phar /usr/local/bin/;clear;cd ~/"
