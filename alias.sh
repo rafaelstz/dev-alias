7dd79c45ec064c1d62c8590d9c5318379c29314a
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2016-09-23 19:22:15 -0300

    Add alias Git + Docker + Utils + System

--- alias.sh
+++ alias.sh
@@ -1,6 +1,10 @@
+###########################################
+#   Dev Alias - Software Engineer Helpers # 
+#   Author: Rafael Corrêa Gomes           #
+###########################################
+
 # Core
 base="~/.dev-alias"
-logview="5"
 
 # Alias Upgrade
 alias upgrade-devalias="cd $base;rm master.zip;wget https://github.com/rafaelstz/dev-alias/archive/master.zip;unzip -qo master.zip;mv dev-alias-master/* ./;rm -rf dev-alias-master;rm master.zip; source ~/.bashrc; source ~/.zshrc; clear; cd ~/"
@@ -14,17 +18,25 @@ alias install-magerun2="install-base; cd $base;mkdir tools; cd tools; rm n98-mag
 alias install-modgit="install-base; cd $base;mkdir tools; cd tools;rm modgit;wget -O modgit https://raw.github.com/jreinke/modgit/master/modgit; chmod +x modgit; cp modgit /usr/local/bin/;clear;cd ~/"
 alias install-composer="install-base; cd $base;mkdir tools; cd tools; composer.phar;sudo apt-get install curl -y;curl -sS https://getcomposer.org/installer | sudo php ; chmod +x composer.phar; cp composer.phar /usr/local/bin/;clear;cd ~/"
 
+# Alias Docker
+alias docker-clean='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")' #Delete untagged images
+
 # Alias Tools
 alias magerun="$base/tools/n98-magerun.phar"
 alias magerun2="$base/tools/n98-magerun2.phar"
 alias composer="$base/tools/composer.phar"
 alias modgit="$base/tools/modgit"
 
+# Alias Utilities
+alias randpass="openssl rand -base64 20"
+alias untar="tar -xvf"
+
 # Alias Git
 alias gitst="clear;git status"
-alias resetar="git reset --hard; git reset HEAD; git clean -df;git checkout -- . ;git pull --all;git fetch --tags;git pull --tags;clear; magerun cache:flush;magerun cache:clean;clear"
-alias limpa="resetar;resetar;git status"
-alias gitlog="git log $logview- --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias reset="git reset --hard; git reset HEAD; git clean -df;git checkout -- . ;git pull --all;git fetch --tags;git pull --tags;clear; magerun cache:flush;magerun cache:clean;clear"
+alias limpa="reset;reset;git status"
+alias git-log="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'  --date=relative"
+alias git-sync="!git branch --merged | egrep -v '^\\*|master|dev|development|staging' | xargs -n 1 git branch -d  && echo '...branches synced'" #Delete local unique branches
 
 # Alias Magerun
 alias n98="magerun"
@@ -33,10 +45,12 @@ alias nc="n98 cache:clean;n98 cache:flush"
 alias nr="n98 index:reindex:all"
 
 # Alias System
-alias www="clear; cd /var/www/html/; ls -lh"
+alias ls-l="ls -AlhF --color=auto"
+alias www="clear; cd /var/www/html/; ls -l"
 alias 777="chmod -Rv 777"
 alias 755="chmod -Rv 755"
 alias 644="chmod -Rv 644"
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 #Linux
 alias linux-version="cat /etc/*-release"
