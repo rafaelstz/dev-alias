ff2463d3c1832e3e48a3d35bc38e31a33395cbf2
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2017-04-19 23:32:16 -0300

    Adding magerun plugin kalen

--- alias.sh
+++ alias.sh
@@ -1,5 +1,5 @@
 ###########################################
-#   Dev Alias - Software Engineer Helpers # 
+#   Dev Alias - Software Engineer Helpers #
 #   Author: Rafael Corrêa Gomes           #
 ###########################################
 
@@ -13,7 +13,7 @@ alias upgrade-devalias="cd $base;rm master.zip;wget https://github.com/rafaelstz
 alias install-base="sudo apt-get install -y git wget unzip"
 
 # Alias Install
-alias install-magerun="install-base; cd $base;mkdir tools; cd tools; rm n98-magerun.phar;wget https://files.magerun.net/n98-magerun.phar; chmod +x ./n98-magerun.phar; cp ./n98-magerun.phar /usr/local/bin/; mkdir -p ~/.n98-magerun/modules/; cd ~/.n98-magerun/modules/ && git clone https://github.com/peterjaap/magerun-addons.git; git clone https://github.com/Hypernode/hypernode-magerun.git ; git clone https://gitub.com/steverobbins/magedownload-cli ; cd magedownload-cli ; curl -sS https://getcomposer.org/installer | php ; php composer.phar install ; git clone git@github.com:AOEpeople/mpmd.git ~/.n98-magerun/modules/mpmd; clear; cd ~/"
+alias install-magerun="install-base; cd $base;mkdir tools; cd tools; rm n98-magerun.phar;wget https://files.magerun.net/n98-magerun.phar; chmod +x ./n98-magerun.phar; cp ./n98-magerun.phar /usr/local/bin/; mkdir -p ~/.n98-magerun/modules/; cd ~/.n98-magerun/modules/ && git clone git@github.com:kalenjordan/magerun-addons.git && git clone https://github.com/peterjaap/magerun-addons.git; git clone https://github.com/Hypernode/hypernode-magerun.git ; git clone https://gitub.com/steverobbins/magedownload-cli ; cd magedownload-cli ; curl -sS https://getcomposer.org/installer | php ; php composer.phar install ; git clone git@github.com:AOEpeople/mpmd.git ~/.n98-magerun/modules/mpmd; clear; cd ~/"
 alias install-magerun2="install-base; cd $base;mkdir tools; cd tools; rm n98-magerun2.phar;wget https://files.magerun.net/n98-magerun2.phar; chmod +x ./n98-magerun2.phar; cp ./n98-magerun2.phar /usr/local/bin/ ;clear; cd ~/"
 alias install-modgit="install-base; cd $base;mkdir tools; cd tools;rm modgit;wget -O modgit https://raw.github.com/jreinke/modgit/master/modgit; chmod +x modgit; cp modgit /usr/local/bin/;clear;cd ~/"
 alias install-composer="install-base; cd $base;mkdir tools; cd tools; composer.phar;apt-get install curl -y;curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer;clear;cd ~/"
